5fd035c15a96474ed5ee18236a0d66ffad20192d
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:45:01 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "data_factory_linked_service_kustos" {
+  description = "All data_factory_linked_service_kusto resources"
+  value       = azurerm_data_factory_linked_service_kusto.data_factory_linked_service_kustos
+}
+
